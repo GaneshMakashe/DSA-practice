d28fe3b38c990211faea423e7329f6979948033c
LENOVO <LENOVO@GaneshMakashe>
2022-06-13 21:35:06 +0530

    practice

--- DSA/src/paidAnujBhaiyaAssisgnments/MoveZerosToEnd.java
+++ DSA/src/paidAnujBhaiyaAssisgnments/MoveZerosToEnd.java
@@ -11,12 +11,31 @@ public class MoveZerosToEnd {
 	
 	//logic not understood 
 	public static void MoveZerosToEnd(int[] a) {
-		int count=0;
-		for (int i = 0; i < a.length; i++)
-	        if (a[i] != 0)
-	            a[count++] = a[i];
-		  for (int i = count; i<a.length;i++)
-		        a[i]=0;
+//		int count=0;
+//		for (int i = 0; i < a.length; i++)
+//	        if (a[i] != 0)
+//	            a[count++] = a[i];
+//		  for (int i = count; i<a.length;i++)
+//		        a[i]=0;
+		int n = a.length;
+		if(n==0 || n==1) {
+			return;
+		}
+		int left=0, right=0;
+		int temp;
+		
+		while(right<n) {
+			if(a[right]==0) 
+			++right;
+			else {
+				temp=a[left];
+				a[left]=a[right];
+				a[right]=temp;
+				++left;
+				++right;
+			}
+			
+		}
 	}
 
 	public static void main(String[] args) {
