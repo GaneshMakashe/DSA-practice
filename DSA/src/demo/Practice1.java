f099282ac8ee1753b27796b1710ddb54eb1ad9a5
GaneshMakashe <ganesh.svk10@gmail.com>
2022-08-28 10:07:30 +0530

    commit

--- DSA/src/demo/Practice1.java
+++ DSA/src/demo/Practice1.java
@@ -1,7 +1,23 @@
 package demo;
 
-public class Practice1 {
+import java.time.LocalDate;
 
+import queueBasics.MainClass;
+
+public class Practice1 {
 	
+	
+
+	public static void main(String[] args) {
+	LocalDate l=LocalDate.of(2019, 3, 17);
+	l=l.minusDays(18);
+	l.minusMonths(1);
+	
+	
+	
+	System.out.println(l);
+	
+	
+	}
 
 }
