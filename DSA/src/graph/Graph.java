5eea6c98e5530082770df335f6e79d5f9369f589
GaneshMakashe <ganesh.svk10@gmail.com>
2022-09-15 13:00:36 +0530

    15th September

---
+++ DSA/src/graph/Graph.java
@@ -0,0 +1,29 @@
+package graph;
+
+public class Graph {
+
+	int a[][];
+	int v;
+	
+	public Graph(int v) {
+		this.v=v;
+		a=new int[v][v];
+	}
+	
+	public void addEdge(int src, int dest) {
+		a[src][dest]=1;
+		a[dest][src]=1;
+	}
+	
+	public static void main(String[] args) {
+		int v=4;
+		Graph graph=new Graph(v);
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(2, 3);
+		graph.addEdge(0, 3);
+		graph.addEdge(2, 1);
+		
+		System.out.println(graph);
+	}
+}
---
+++ DSA/src/graph/Graph2.java
@@ -0,0 +1,121 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Graph2 {
+
+	static ArrayList<ArrayList<Integer>> a= new ArrayList<ArrayList<Integer>>();
+	int v;
+	
+	public Graph2(int v) {
+		this.v=v;
+		for(int i=0;i<v;i++) {
+			a.add(new ArrayList<Integer>());
+		}
+	}
+	
+	public void addEdge(int src, int dest) {
+		a.get(src).add(dest);
+		a.get(dest).add(src);
+	}
+	
+	
+	public static void dfs(int src,boolean vis[]) {
+		vis[src]=true;
+		System.out.print(src+" ");
+		
+		for(Integer neighbor: a.get(src)) {
+			if(!vis[neighbor]) {
+				dfs(neighbor,vis);
+			}
+		}
+	}
+	
+	public static boolean isCyclePresent(int src, boolean vis[], int parent) {
+		vis[src]=true;
+		
+		for(Integer neighbor: a.get(src)) {
+			if(!vis[neighbor]) {
+				if(isCyclePresent(neighbor, vis, src)) return true;
+			}else if(neighbor !=parent){
+				 return true;
+			}
+		}
+		return false;
+	}
+	
+	//remove dis to use code as bfs code 
+	public static void bfs(int src,boolean vis[], int dis[]) {
+		Queue<Integer> q=new LinkedList<>();
+		q.add(src);
+		vis[src]=true;
+		dis[src]=0;
+		
+		while(!q.isEmpty()) {
+			int cur=q.poll();
+			System.out.print(cur+" ");
+			
+			for(Integer neighbor:a.get(cur)) {
+				if(!vis[neighbor]) {
+					dis[neighbor]=dis[cur]+1;
+					q.add(neighbor);
+					vis[neighbor]=true;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int v=8;
+		Graph2 graph=new Graph2(v);
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(2, 3);
+		graph.addEdge(0, 3);
+		graph.addEdge(3, 6);
+		graph.addEdge(5, 7);
+		graph.addEdge(2, 7);
+		graph.addEdge(4, 5);
+		graph.addEdge(5, 6);
+		graph.addEdge(4, 6);
+		
+		boolean vis[]=new boolean[v];
+//		boolean vis1[]=new boolean[v];
+//		dfs(0,vis);
+//		System.out.println("");
+//		bfs(0,vis1);
+		
+		//dfs for every connected componenet
+//		int count=0;
+//		for(int i=0;i<v;i++) {
+//			if(!vis[i]) {
+//				count++;
+//				dfs(i,vis);
+//			}
+//		}
+//		System.out.println(" ");
+//		System.out.println(count);
+		
+		//shortest path in graph
+//		int dis[]=new int[v];
+//		Arrays.fill(dis, Integer.MAX_VALUE);
+//		
+//		bfs(0,vis,dis);
+//		System.out.println("");
+//		for(int i=0;i<v;i++) {
+//			System.out.println(i+" "+dis[i]);
+//		}
+		
+		//detect cycle
+		for(int i=0;i<v;i++) {
+			if(!vis[i]) {
+				if(isCyclePresent(i, vis, -1)) {
+					System.out.println("cycle found");
+				}
+			}
+		}
+	}
+}
